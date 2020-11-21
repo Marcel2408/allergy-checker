a4e4b6e9e5e023932fba5e9a0373cb7995b802a1
Charles-Thibault <charles.thibault.verrier@gmail.com>
2020-11-21 12:13:39 +0100

    fix(navbar test): fix of the imports

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,9 +1,4 @@
-
-import { DebugElement } from '@angular/core';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import { NavigationEnd, Router } from '@angular/router';
@@ -46,20 +41,25 @@ describe('NavbarComponent', () => {
     console.log(singOutButton)
     expect(singOutButton.textContent).toEqual('Sign Out');
   });
+
   it('isLoading should be true at first', () => {
     expect(component.isLoading).toBe(true, 'true at first');
-  })
+  });
+
   it('isLoading should be false after 2s', fakeAsync(() => {
     let component = new NavbarComponent();
     expect(component.isLoading).toBe(true)
     component.ngOnInit()
     tick(2000)
     expect(component.isLoading).toBeFalse()
-  }))
+  }));
+
   it('Sign Out should redirect to the login path', fakeAsync(() => {
     component.isLoading = false;
     fixture.detectChanges();
     const link = fixture.debugElement.query(By.css('.signout_link'));
     tick();
     console.log(link);
+}));
+
 });
